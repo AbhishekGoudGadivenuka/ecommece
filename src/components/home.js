e826b717ed37b6e6d0e53d70a589711c862bd109
AbhishekGoudGadivenuka <abhishekgoudgadivenuka@gmail.com>
2024-07-09 19:21:21 +0530

    repo

---
+++ src/components/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.css'
+const Home = () => {
+  return (
+    <div className="container">
+      <h2>Welcome to our Abhishek Website</h2>
+      <p>Explore our products and services.</p>
+
+      <p className='foot'>ALL CREDITS ARE RESERVED TO @ 22071A0518</p>
+    </div>
+  );
+};
+
+export default Home;
